ff11d239b76266f6fad550cc431bdd8b5ff9df72
Hayden <hayden.ian@outlook.com>
2019-01-01 17:24:44 -0600

    Finished Lesson 7-1

--- Lesson 7-1.py
+++ Lesson 7-1.py
@@ -1,1 +1,14 @@
-#Import Python packages
+# Import Python packages
+
+# How to Import Packages:
+# Import module the simple way:
+import package_name
+# Now, all functions and objects from the package must be qualified to be used:
+package_name.func_name()
+
+# Importing Individual Functions:
+# To importat one function at a time:
+from package import func_name
+# To import all functions and objects:
+from package import *
+# The problem with these approaches is that they make "name conflicts" likely.
